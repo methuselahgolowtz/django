3152feffb28f7c4326988a033d714a1074b48a6b
Ucsaf-wifi <ucsaf-wifi@Ucsaf-wifi>
2024-07-16 12:54:14 -0400

    project

--- clothScale/views.py
+++ clothScale/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+import joblib
+import pandas as pd
 # Create your views here.
+def home(request):
+    if request.method == "POST":
+        weight = request.POST['weight']
+        height = request.POST['height']
+        age = request.POST['Age']
+        
+        model_data = joblib.load("clothScale/mymodel.joblib")#This stae tement its is used to lead the model
+        model = model_data['model']
+        label_data = model_data['label_data']
+        
+        new_data = [[weight, height, age]]  
+        new_data_df = pd.DataFrame(data=new_data, columns=["weight", "height", "age"])  
+        predicted_size = model.predict(new_data_df)[0]
+        predicted_size_label = label_data.inverse_transform([predicted_size])[0]
+        return render(request, "clothScale/templates/clothScale/index.html", {"predicted_size":predicted_size_label})
+
+    else:
+        return render(request, "clothScale/templates/clothScale/index.html")
--- scale/settings.py
+++ scale/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clothScale',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'scale.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,11 +80,15 @@ WSGI_APPLICATION = 'scale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+#this is used for online purpose 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(env("DATABASE_URL"))
 }
 
 
